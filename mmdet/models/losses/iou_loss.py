b3e4485f92d44240aaff44afaad1ea7cd7359037
k0kumar <72912657+k0kumar@users.noreply.github.com>
2025-04-29 15:34:02 +0530

    Update iou_loss.py

--- mmdet/models/losses/iou_loss.py
+++ mmdet/models/losses/iou_loss.py
@@ -9,7 +9,7 @@ from ..builder import LOSSES
 from .utils import weighted_loss
 
 
-@mmcv.jit(derivate=True, coderize=True)
+@mmcv.utils.jit(derivate=True, coderize=True)
 @weighted_loss
 def iou_loss(pred, target, linear=False, eps=1e-6):
     """IoU loss.
@@ -36,7 +36,7 @@ def iou_loss(pred, target, linear=False, eps=1e-6):
     return loss
 
 
-@mmcv.jit(derivate=True, coderize=True)
+@mmcv.utils.jit(derivate=True, coderize=True)
 @weighted_loss
 def bounded_iou_loss(pred, target, beta=0.2, eps=1e-3):
     """BIoULoss.
@@ -82,7 +82,7 @@ def bounded_iou_loss(pred, target, beta=0.2, eps=1e-3):
     return loss
 
 
-@mmcv.jit(derivate=True, coderize=True)
+@mmcv.utils.jit(derivate=True, coderize=True)
 @weighted_loss
 def giou_loss(pred, target, eps=1e-7):
     r"""`Generalized Intersection over Union: A Metric and A Loss for Bounding
@@ -102,7 +102,7 @@ def giou_loss(pred, target, eps=1e-7):
     return loss
 
 
-@mmcv.jit(derivate=True, coderize=True)
+@mmcv.utils.jit(derivate=True, coderize=True)
 @weighted_loss
 def diou_loss(pred, target, eps=1e-7):
     r"""`Implementation of Distance-IoU Loss: Faster and Better
@@ -157,7 +157,7 @@ def diou_loss(pred, target, eps=1e-7):
     return loss
 
 
-@mmcv.jit(derivate=True, coderize=True)
+@mmcv.utils.jit(derivate=True, coderize=True)
 @weighted_loss
 def ciou_loss(pred, target, eps=1e-7):
     r"""`Implementation of paper `Enhancing Geometric Factors into
