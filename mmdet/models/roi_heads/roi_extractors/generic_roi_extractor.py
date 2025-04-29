f6fcd6bc0c06fafaadd80cc6a408bc1b6a3a7bfd
k0kumar <72912657+k0kumar@users.noreply.github.com>
2025-04-29 17:18:04 +0530

    Update generic_roi_extractor.py

--- mmdet/models/roi_heads/roi_extractors/generic_roi_extractor.py
+++ mmdet/models/roi_heads/roi_extractors/generic_roi_extractor.py
@@ -1,5 +1,5 @@
 from mmcv.cnn.bricks import build_plugin_layer
-from mmcv.runner import force_fp32
+from mmdet.utils.fp16_utils import force_fp32
 
 from mmdet.models.builder import ROI_EXTRACTORS
 from .base_roi_extractor import BaseRoIExtractor
