bcd5b5d6fd403c8dc22a35eebb039279bdc5bae0
k0kumar <72912657+k0kumar@users.noreply.github.com>
2025-04-29 15:05:39 +0530

    Update anchor_head.py

--- mmdet/models/dense_heads/anchor_head.py
+++ mmdet/models/dense_heads/anchor_head.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from mmengine.model import normal_init
-from mmcv.runner import force_fp32
+from mmdet.utils.fp16_utils import force_fp32
 
 from mmdet.core import (anchor_inside_flags, build_anchor_generator,
                         build_assigner, build_bbox_coder, build_sampler,
