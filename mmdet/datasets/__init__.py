58051f8e60f3cd7e6f027fe378826bdaf411f5fa
k0kumar <72912657+k0kumar@users.noreply.github.com>
2025-04-29 13:43:57 +0530

    Update __init__.py

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -1,17 +1,17 @@
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
-from .cityscapes import CityscapesDataset
+# from .cityscapes import CityscapesDataset
 from .coco import CocoDataset
-from .custom import CustomDataset
+# from .custom import CustomDataset
 from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
                                RepeatDataset)
-from .deepfashion import DeepFashionDataset
-from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
+# from .deepfashion import DeepFashionDataset
+# from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
 from .utils import (NumClassCheckHook, get_loading_pipeline,
                     replace_ImageToTensor)
-from .voc import VOCDataset
-from .wider_face import WIDERFaceDataset
-from .xml_style import XMLDataset
+# from .voc import VOCDataset
+# from .wider_face import WIDERFaceDataset
+# from .xml_style import XMLDataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
