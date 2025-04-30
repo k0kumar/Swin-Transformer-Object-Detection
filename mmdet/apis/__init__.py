6cc5d3cab40614fd6bb85e8f6d48d5c91baa052a
k0kumar <72912657+k0kumar@users.noreply.github.com>
2025-04-30 13:24:38 +0530

    Update __init__.py

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,9 +1,8 @@
 from .inference import inference_detector,init_detector
-from .test import multi_gpu_test, single_gpu_test
-from .train import get_root_logger, set_random_seed, train_detector
+# from .test import multi_gpu_test, single_gpu_test
+# from .train import get_root_logger, set_random_seed, train_detector
 
 __all__ = [
-    'get_root_logger', 'set_random_seed', 'train_detector', 'init_detector',
-    'inference_detector',
-    'multi_gpu_test', 'single_gpu_test'
+     'init_detector',
+    'inference_detector'  
 ]
